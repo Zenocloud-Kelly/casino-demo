ba80b1e34c42676ad3a48ba6d2adcfc216bc6632
Zenocloud-Kelly <zenocloudkelly@gmail.com>
2024-10-15 17:11:22 +0800

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,9 +13,9 @@ const RESOURCES = {"canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
 "canvaskit/chromium/canvaskit.js": "671c6b4f8fcc199dcc551c7bb125f239",
 "canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
-"index.html": "e9ad6c8592a93f31287b0f1bd651cfae",
-"/": "e9ad6c8592a93f31287b0f1bd651cfae",
-"hls_player.html": "79a699df6d4879e87f4716539d219f3a",
+"index.html": "33ae5e6d2f5ffc89d343013f951c2f42",
+"/": "33ae5e6d2f5ffc89d343013f951c2f42",
+"hls_player.html": "7cdb131aa885cc7bf3b50ad7976fa846",
 "version.json": "f1b56ff9ea17ae46138d2cc702e9ac76",
 "main.dart.js": "8b541455df4f783cc4d00fd1ddf6f4b0",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
@@ -151,7 +151,7 @@ const RESOURCES = {"canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "assets/assets/background.png": "889cf3870880f696ceca64be3fc7f3ad",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "flv_player.html": "823ca9118427af14f9c8fa5743f4831f",
-"flutter_bootstrap.js": "e4f51b6640c360ab5c04c287a8db1b9c"};
+"flutter_bootstrap.js": "02c166eec9d1517dc6416e8ad9723400"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
