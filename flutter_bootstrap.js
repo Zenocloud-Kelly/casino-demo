8cf3df0d2e9dc77ff6f27e6b5996f25478a46f41
Zenocloud-Kelly <zenocloudkelly@gmail.com>
2024-10-17 12:31:03 +0800

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4119094622"
+    serviceWorkerVersion: "3719903532"
   }
 });
